52262f4b78d75bf87ac0a9e7cc034e266987eb3f
40823119 <40823119@gm.nfu.edu.tw>
2021-03-18 04:03:51 +0800

    leo

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'cmstemplate user'
-SITENAME = 'cmstemplate blog'
+AUTHOR = 'stagel-agl1'
+SITENAME = 'stagle-ag11'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
